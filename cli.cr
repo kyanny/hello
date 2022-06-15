2720f82daa48c71185656262d79dcf73c353b3ef
Kensuke Nagae <kyanny@github.com>
2022-06-15 14:07:18 +0000

    impl

--- cli.cr
+++ cli.cr
@@ -2,6 +2,10 @@ require "option_parser"
 
 OptionParser.parse do |parser|
   parser.banner = "WELCOME TO THE CLI!"
+  parser.on "-n NAME", "--name=NAME", "NAME" do |name|
+    puts "Hi #{name}"
+    exit
+  end
   parser.on "-v", "--version", "SHOW VERSION" do
     puts "1.0.0"
     exit
@@ -9,6 +13,15 @@ OptionParser.parse do |parser|
   parser.on "-h", "--help", "SHOW HELP" do
     puts parser
     exit
+  end
+  
+  parser.missing_option do |option_flag|
+    puts "ERROR: #{option_flag} is missing"
+    exit 254
+  end
+  parser.invalid_option do |option_flag|
+    puts "ERROR: #{option_flag} is invalid"
+    exit 255
   end
 end
 
